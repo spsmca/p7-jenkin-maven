b3778b9fdcb23a80c1a060391ce806d9dd3bc149
spsmca <sps_mca@rediffmail.com>
2021-04-24 16:36:44 +0530

    initial commit or Push

---
+++ src/main/java/com/jenkin/maven/demo/JenkinMavenDemo.java
@@ -0,0 +1,13 @@
+package com.jenkin.maven.demo;
+
+public class JenkinMavenDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Begin===>JenkinMavenDemo.main()....");
+		for(int i=1; i<6; ++i) {
+			System.out.println("Hey Philip===> counter=" + i);
+		}
+		System.out.println("End===>JenkinMavenDemo.main()....");
+	}
+
+}
